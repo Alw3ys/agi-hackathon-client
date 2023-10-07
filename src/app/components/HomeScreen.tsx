deb6cdb17db4672e35233111c0f8cb6b288f239e
codyswain <swain.cody@gmail.com>
2023-10-07 15:44:38 -0700

    initialized

---
+++ src/app/components/HomeScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Homescreen: React.FC<any> = () => {
+  return (<></>);
+}
+
+export default Homescreen;
---
+++ src/app/components/PatientSelectionScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PatientSelectionScreen: React.FC<any> = () => {
+  return <div className="w-screen h-screen bg-green-500"></div>;
+}
+
+export default PatientSelectionScreen;
